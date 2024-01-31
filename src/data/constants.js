45a820e3ee5cecc3ce2750d7faab54770d449b01
amit0-01 <amit22414@gmail.com>
2024-01-31 15:04:06 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,165 @@
+export const Bio = {
+  name: "Amit kumar",
+  roles: [
+    "Full Stack Developer",
+    "Software Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/amit0-01",
+  resume:
+    "https://drive.google.com/file/d/1f39wsQtaa3Q_1P-YtoC5jrZ-wVGhUCTU/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/amit-kumar-2b69a21b8/",
+  twitter: "https://twitter.com",
+  insta: "https://www.instagram.com",
+  facebook: "https://www.facebook.com",
+};
+
+export const skills = [
+  {
+    skills: [
+      
+      {
+        name: "C",
+        image:
+        "https://upload.wikimedia.org/wikipedia/commons/1/18/ISO_C%2B%2B_Logo.svg"     
+       },
+
+       {
+        name: "C++",
+        image:
+        "https://upload.wikimedia.org/wikipedia/commons/1/19/C_Logo.png"       },
+
+
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+     
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://private-user-images.githubusercontent.com/145347884/301066989-7f5bcdfe-3f37-496b-a3e7-3c4780c9bbb0.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDY2ODQ0OTQsIm5iZiI6MTcwNjY4NDE5NCwicGF0aCI6Ii8xNDUzNDc4ODQvMzAxMDY2OTg5LTdmNWJjZGZlLTNmMzctNDk2Yi1hM2U3LTNjNDc4MGM5YmJiMC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwMTMxJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDEzMVQwNjU2MzRaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT02OWM2MDhjMDc3YTM0ZTI5NWE3ZjJiNjNhOGNhNWM3YzBmMDYwYmU0MjAwNzQ0NDA1ZDEyZjliZGNiNTNjZDBmJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.mmCjtjxHKRUqOB9m8_4pHI9zjpNFyHdxQA0m7dd09BU",
+    role: "Java delveloper Intern",
+    company: "Allsoft solutions",
+    date: "July 2022 - Aug 2022",
+    desc: "LEARNED SOME BASIC AND ADVANCED CONCEPTS OF JAVA.BUILT A PROJECT OF ONLINE ELECTRIC SHOP USING JAVA, HTML ,CSS JAVASCRIPT AND SQL",
+    skills: [
+      "Learned core java concepts."
+    ],
+    doc: "https://drive.google.com/file/d/16MV0JwryNpcyJ2_qdRPgpqHFMsqSQJQp/view?usp=sharing",
+    img:"https://private-user-images.githubusercontent.com/145347884/301066989-7f5bcdfe-3f37-496b-a3e7-3c4780c9bbb0.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDY2ODQ0OTQsIm5iZiI6MTcwNjY4NDE5NCwicGF0aCI6Ii8xNDUzNDc4ODQvMzAxMDY2OTg5LTdmNWJjZGZlLTNmMzctNDk2Yi1hM2U3LTNjNDc4MGM5YmJiMC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwMTMxJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDEzMVQwNjU2MzRaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT02OWM2MDhjMDc3YTM0ZTI5NWE3ZjJiNjNhOGNhNWM3YzBmMDYwYmU0MjAwNzQ0NDA1ZDEyZjliZGNiNTNjZDBmJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.mmCjtjxHKRUqOB9m8_4pHI9zjpNFyHdxQA0m7dd09BU",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Kiit.jpeg?alt=media&token=3839d520-c59d-4341-ad8f-0980c98de4dd",
+    school: "Punjabi University Patiala.",
+    date: "2020 - 2024",
+    grade: "7.84 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Sciend and Engineering at Punjabi University , Patiala. I have completed 7 semesters and have a CGPA of 7.84. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.I'm working on exciting projects .",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+    school: "Senior Secondary Model School, Punjabi University, Patiala.",
+    date: "2019 - 2020",
+    grade: "92%",
+    desc: "I completed my class 12 high school education at Senior Secondary Model School, Punjabi University, Patiala., where I studied Non-medical.",
+    degree: "XII, Non-medical",
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+    school: "Senior Secondary Model School, Punjabi University, Patiala.",
+    date: "2017 - 2018",
+    grade: "86%",
+    desc: "I completed my class 10 education at Senior Secondary Model School, Punjabi University, Patiala. ",
+    degree: "X",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "Interactive music player application	",
+    description:
+      "Designed and developed an interactive web application that allows users to play and manage a collection of songs with user-friendly controls.The project can play and manage collection of songs. The features is it has progress bar, play/pause,previous and next controls.Built using HTML, CSS and Javascript.",
+    image: 
+      "https://private-user-images.githubusercontent.com/145347884/301050428-d8e0ace6-033c-40f4-911d-f73e80507a08.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDY2Nzk4MDMsIm5iZiI6MTcwNjY3OTUwMywicGF0aCI6Ii8xNDUzNDc4ODQvMzAxMDUwNDI4LWQ4ZTBhY2U2LTAzM2MtNDBmNC05MTFkLWY3M2U4MDUwN2EwOC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwMTMxJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDEzMVQwNTM4MjNaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0zM2Y0MGIyMzdhY2Q3M2Q3ZWUzYWNlOTE1ZjMyMjg3OGM3ZTY0Y2Q4YWZlNTc2OTkwZWVlYzZmNzllYmRlYmY5JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.Y-Xfn_LyMu_QzSNG2ZB57LCbTEPEStssOw6qy7QMX9E", 
+    tags: [
+      "HTML",
+      "CSS",
+      "JAVASCRIPT"
+    ],
+    category: "web app",
+    github: "https://github.com/amit0-01/MySpotify",
+    webapp: "https://myspotify-by-amit.netlify.app",
+  },
+  {
+    id: 9,
+    title: "Lighten Electricals	",
+    description:
+      "An e-commerce website for electrical products.Utilized Java for backend development for user authentication, and database operations. Mysql database is used to store and manage product information, user data, and transaction records.Built using HTML,CSS ,Java and sql.",
+    image:
+      "https://private-user-images.githubusercontent.com/145347884/301056205-cee02eaa-2b2a-454d-a22d-2e329b7eb8fb.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDY2ODE1NjksIm5iZiI6MTcwNjY4MTI2OSwicGF0aCI6Ii8xNDUzNDc4ODQvMzAxMDU2MjA1LWNlZTAyZWFhLTJiMmEtNDU0ZC1hMjJkLTJlMzI5YjdlYjhmYi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwMTMxJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDEzMVQwNjA3NDlaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT02MzZhZDAyNmM5MmMyYzNjYzQyYjFlMjUwODc1MDljN2UxNTE2YTY2ZDBhY2RjNzY1NGRhMWExNDk0ZDIxZDljJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.1vHpEY4E8vu1V_O5dRyRApEkziAnZn-NJR5fafCQS_s",
+    tags: [
+      "JAVA",
+      "HTML",
+      "CSS", 
+      "JAVASCRIPT"
+    ],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 0,
+    title: "tic tac toe game",
+    description:
+      "Created tic tac toe game with beautiful ui design.Implemented game logic to dynamically check for winning patterns and display the winner.Incorporated reset and new game buttions for seamless game control. Built using html, css and javascript",
+      image:
+      "https://private-user-images.githubusercontent.com/145347884/301057150-85e55dc4-c2c4-4661-89c0-53dd9b238365.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDY2ODE3ODQsIm5iZiI6MTcwNjY4MTQ4NCwicGF0aCI6Ii8xNDUzNDc4ODQvMzAxMDU3MTUwLTg1ZTU1ZGM0LWMyYzQtNDY2MS04OWMwLTUzZGQ5YjIzODM2NS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwMTMxJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDEzMVQwNjExMjRaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1kMjVhZDkyYzdhOTcyYzk0Y2U4ZmNiZmFkN2RiYWE5YzI2ZmM1MTk4NzFjMDdkNzhiMWQ3ZjJmNzBiZjQyMTQ1JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.EwFCPxAhT-w609j6j0k2j9Ac7su2LsTRgJUt7JzNqEc",
+    tags: ["JAVASCRIPT", "HTML", "CSS",],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];

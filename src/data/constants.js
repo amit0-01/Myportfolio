9153d67ccdef77ea301a4754004c584db63823d1
amit0-01 <amit22414@gmail.com>
2024-03-31 14:02:29 +0530

    resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/amit0-01",
   resume:
-    "https://drive.google.com/file/d/1ljVPzKn4wTIIjEqN8Ph8tHQTXQPNgs1a/view?usp=sharing",
+    "https://drive.google.com/file/d/1jo_3TPVxwJqSFNPSqtvSDELMYU2XV1Ls/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/amit-kumar-2b69a21b8/",
   twitter: "https://twitter.com",
   insta: "https://www.instagram.com",
